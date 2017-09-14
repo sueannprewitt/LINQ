8cafff166c944541a9756b248f551af3835ff1c1
Sue Ann <sueannprewitt@yahoo.com>
2017-09-14 10:35:21 -0400

    Initial commit using LINQ and lambda syntax

--- LINQ/Program.cs
+++ LINQ/Program.cs
@@ -3,13 +3,41 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Diagnostics;
+using EducationLibrary;
 
 namespace LINQ
 {
     class Program
     {
+        void Run()
+        {
+            var students = StudentCollection.Select();
+
+            //to get students with GPA >= 3.5 and SAT > 1400 in GPA descending sequence without LINQ:
+            // StudentCollection DeansListStudents = new StudentCollection();
+            // foreach (var s in student) {
+            // if(s.GPA >= 3.5 && s.SAT > 1400){
+            //     DeansListStudents.Add(s);
+            // }
+            // }
+
+
+            //this is using LINQ:
+            var TopStudents = students.Where(stud => stud.GPA >= 3.5 && stud.SAT > 1400).OrderByDescending(s => s.GPA);     
+
+
+
+
+            foreach(var student in TopStudents)
+            {
+                Debug.WriteLine($"{student.FirstName} {student.LastName} GPA is {student.GPA} and SAT is {student.SAT}");
+            }
+
+        }
         static void Main(string[] args)
         {
+            new Program().Run();
         }
     }
 }
